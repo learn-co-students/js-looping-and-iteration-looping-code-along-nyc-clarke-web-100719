18bb8e58d748e86d3d6a456a5682b1a5a1a62967
DannyBrito <britodanny1997@gmail.com>
2019-11-17 12:33:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+
+function writeCards(arr){
+    let cards =[];
+    for(let i = 0;i < arr.length; i++) cards.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    return cards;
+};
+
+function countDown(numb){
+    while(numb >= 0){
+        console.log(numb--);
+    }
+}
