08ab46108342c43824ae85abaeeca12fc339ea86
gnardinosaur <github email address>
2019-11-17 17:11:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    let answer = []
+    for (let i = 0; i < arr.length; i++) {
+        answer.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return answer;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
