3d0485a060fffbd9db5c3f63474a6865536c7d6d
TJBender <tiana.bender01@gmail.com>
2019-11-17 23:16:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+//Code your solution below!
+
+function writeCards(arr, ) {
+    let messages = [];
+    for(let i = 0; i < arr.length; i++) {
+        messages.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(i)
+        i++
+    }
+}
