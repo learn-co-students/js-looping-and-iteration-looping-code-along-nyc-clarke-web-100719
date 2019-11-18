198317b7fa7ecc7a329594838ea05f627d6da012
GeneHFang <ghl234@nyu.edu>
2019-11-18 00:13:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(nameArr){
+
+    let x = nameArr.length;
+    let ret = new Array();
+    for (let i = 0; i < x ; i++)
+    {
+        ret.push(`Thank you, ${nameArr[i]}, for the wonderful surprise gift!`);
+    }
+    return ret;
+
+}
+
+function countDown(number){
+
+    do {
+
+        console.log(number);
+
+        number--;
+    } while (number > -1);
+
+}
