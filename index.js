f619b9858cc07eecdaf991a618d12c2a04b2f148
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-18 11:45:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// const writeCards = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(arr) {
+    let newArr = [];
+
+    for(let i=0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr;
+}
+
+
+function countDown(num) {
+    // for (let i = 0; i < num; i++) {
+    //     console.log(num);
+    //     num--;
+    // }
+
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+// for(let i=0; i < writeCards.length; i++) {
+//     console.log( `Thank you, ${writeCards}, for the wonderful surprise gift!`);
+// }
